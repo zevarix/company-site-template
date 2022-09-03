7d3bd2dbae052b384c04c467b053f795312375be
Peder Klockmann <peder.klockamnn@gmail.com>
2022-09-02 19:16:19 -0700

    Uploading vendor folder for now - will remove after adding composer tutorial

---
+++ vendor/composer/installed.php
@@ -0,0 +1,47 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '54f0cb590c48c564a545dab59cc7dfbd13c62a8c',
+        'name' => 'pederklockmann/company-site-template',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fortawesome/font-awesome' => array(
+            'pretty_version' => '6.2.0',
+            'version' => '6.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../fortawesome/font-awesome',
+            'aliases' => array(),
+            'reference' => 'd3a7818c253fcbafff9ebd1d4abb2866c192e1d7',
+            'dev_requirement' => false,
+        ),
+        'pederklockmann/company-site-template' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '54f0cb590c48c564a545dab59cc7dfbd13c62a8c',
+            'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => '5.2.0',
+            'version' => '5.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'reference' => 'edf9c40956d19e6ab3f9151bfe0dfac6be06fa21',
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '5.2.0',
+            ),
+        ),
+    ),
+);
